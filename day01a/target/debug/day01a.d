40a7baa2cacd4ac6f990ccecf8ff0a29086eba0a
Peter Savnik <peter@savnik.dk>
2020-10-29 19:20:02 +0100

    complete day01a

---
+++ day01a/target/debug/day01a.d
@@ -0,0 +1,1 @@
+/home/savnik/aoc2019/day01a/target/debug/day01a: /home/savnik/aoc2019/day01a/src/main.rs
---
+++ day01a/target/debug/deps/day01a-6e24176ed050b870.d
@@ -0,0 +1,5 @@
+/home/savnik/aoc2019/day01a/target/debug/deps/day01a-6e24176ed050b870: src/main.rs
+
+/home/savnik/aoc2019/day01a/target/debug/deps/day01a-6e24176ed050b870.d: src/main.rs
+
+src/main.rs:
